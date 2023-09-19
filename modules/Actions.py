5d56e82ce446f0b839e6e01dfe67f5e1b50a15ae
ikubay <ikubaysan@gmail.com>
2023-09-19 04:02:02 -0700

    Use duration intead of speed for movement

--- modules/Actions.py
+++ modules/Actions.py
@@ -65,18 +65,19 @@ class Actions:
         self.action_queue.append(action)
         logger.info(f"Enqueued action: {action.value}")
 
-
     def _execute_actions(self):
         while True:
-            if self.is_window_focused(self.window_title):
-                while self.action_queue:
+            is_focused = self.is_window_focused(self.window_title)
+            if not is_focused:
+                self.action_queue.clear()
+            else:
+                while self.action_queue and is_focused:
                     action = self.action_queue.popleft()
                     getattr(self, action.value)()
                     logger.info(f"Executed action: {action.value}")
-            else:
-                # Clear the queue if the window is not focused
-                self.action_queue.clear()
 
+                    # Re-check focus status before looping again
+                    is_focused = self.is_window_focused(self.window_title)
             time.sleep(1)
 
     @staticmethod
@@ -86,7 +87,7 @@ class Actions:
             active_window = gw.getActiveWindow()
             return title.lower() in active_window.title.lower()
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in is_window_focused(): {e}")
             return False
 
     def press_key(self, hex_key_code):
@@ -103,37 +104,37 @@ class Actions:
         x = Input(ctypes.c_ulong(1), ii_)
         ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-    def move_forward(self):
+    def move_forward(self, duration: float = 0.5):
         self.press_key(W)
-        time.sleep(0.5)
+        time.sleep(duration)
         self.release_key(W)
 
-    def move_back(self):
+    def move_back(self, duration: float = 0.5):
         self.press_key(S)
-        time.sleep(0.5)
+        time.sleep(duration)
         self.release_key(S)
 
-    def turn_left(self):
+    def move_left(self, duration: float = 0.5):
         self.press_key(A)
-        time.sleep(0.5)
+        time.sleep(duration)
         self.release_key(A)
 
-    def turn_right(self):
+    def move_right(self, duration: float = 0.5):
         self.press_key(D)
-        time.sleep(0.5)
+        time.sleep(duration)
         self.release_key(D)
 
-    def move_mouse(self, direction, distance: int = 50, speed: float = 1):
+    def move_mouse(self, direction, distance: int = 50, duration: float = 1):
         """
         :param direction: The direction to move the mouse ('left', 'right', 'up', 'down')
         :param distance: The distance to move the mouse.
-        :param speed: A float between 0 (exclusive) and 1 (inclusive). 1 means instant movement.
+        :param duration: Total time in seconds it should take to move the mouse the specified distance.
         :return: None
         """
-        if speed <= 0 or speed > 1:
-            raise ValueError("Invalid speed value. Must be between 0 and 1 (exclusive).")
+        if duration <= 0:
+            raise ValueError("Invalid duration value. Must be greater than 0.")
 
-        delay = 1 - speed
+        step_delay = duration / distance  # Time to sleep between each step
         step_x, step_y = 0, 0
 
         if direction == 'left':
@@ -149,29 +150,32 @@ class Actions:
 
         for _ in range(distance):
             ctypes.windll.user32.mouse_event(0x0001, step_x, step_y, 0, 0)
-            time.sleep(delay / distance)
+            time.sleep(step_delay)
 
-    def move_mouse_left(self, distance: int = 50, speed: float = 1):
-        self.move_mouse('left', distance, speed)
+    def move_mouse_left(self, distance: int = 50, duration: float = 1):
+        self.move_mouse('left', distance, duration)
 
-    def move_mouse_right(self, distance: int = 50, speed: float = 1):
-        self.move_mouse('right', distance, speed)
+    def move_mouse_right(self, distance: int = 50, duration: float = 1):
+        self.move_mouse('right', distance, duration)
 
-    def move_mouse_up(self, distance: int = 50, speed: float = 1):
-        self.move_mouse('up', distance, speed)
+    def move_mouse_up(self, distance: int = 50, duration: float = 1):
+        self.move_mouse('up', distance, duration)
 
-    def move_mouse_down(self, distance: int = 50, speed: float = 1):
-        self.move_mouse('down', distance, speed)
+    def move_mouse_down(self, distance: int = 50, duration: float = 1):
+        self.move_mouse('down', distance, duration)
 
     def nod_head(self):
-        self.move_mouse_up(distance=250, speed=0.999)
-        self.move_mouse_down(distance=500, speed=0.999)
-        self.move_mouse_up(distance=250, speed=0.999)
+        self.move_mouse_up(distance=250, duration=0.05)
+        self.move_mouse_down(distance=500, duration=0.1)
+        self.move_mouse_up(distance=250, duration=0.05)
 
     def shake_head(self):
-        self.move_mouse_left(distance=250, speed=0.999)
-        self.move_mouse_right(distance=500, speed=0.999)
-        self.move_mouse_left(distance=250, speed=0.999)
+        self.move_mouse_left(distance=250, duration=0.05)
+        self.move_mouse_right(distance=500, duration=0.1)
+        self.move_mouse_left(distance=250, duration=0.05)
+
+    def turn_around(self):
+        self.move_mouse_right(distance=960, duration=5)
 
 
 if __name__ == "__main__":
@@ -181,5 +185,11 @@ if __name__ == "__main__":
     while True:
         if actions.is_window_focused(actions.window_title):
             print("Neos is focused")
+            #actions.enqueue_action(ActionEnum.TURN_AROUND)
+            actions.enqueue_action(ActionEnum.NOD_HEAD)
+            actions.enqueue_action(ActionEnum.NOD_HEAD)
+            time.sleep(2)
             actions.enqueue_action(ActionEnum.SHAKE_HEAD)
-        time.sleep(1)
+            actions.enqueue_action(ActionEnum.SHAKE_HEAD)
+
+        time.sleep(3)
--- modules/enums/ActionEnum.py
+++ modules/enums/ActionEnum.py
@@ -3,7 +3,8 @@ from enum import Enum
 class ActionEnum(Enum):
     MOVE_FORWARD = "move_forward"
     MOVE_BACK = "move_back"
-    TURN_LEFT = "turn_left"
-    TURN_RIGHT = "turn_right"
+    MOVE_LEFT = "move_left"
+    MOVE_RIGHT = "move_right"
     SHAKE_HEAD = "shake_head"
     NOD_HEAD = "nod_head"
+    TURN_AROUND = "turn_around"
